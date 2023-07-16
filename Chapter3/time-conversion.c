c0f8a3287c2cf16d58ca28cdf30f0c4fb0d18849
Otakus0320 <116818128+Otakus0320@users.noreply.github.com>
2023-07-16 21:17:12 +0800

    Create time-conversion.c

---
+++ Chapter3/time-conversion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int time;
+    scanf("%d", &time);
+    int utc;
+    if (time>800){
+        utc = time-800;
+    } else{
+        utc = time + 1600;
+    }
+    printf("utc=%d", utc);
+}
